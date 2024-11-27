3d8122a82b1e7fb539a175fe67aca283f290037f
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-27 15:13:49 +0530

    data displayed on ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import InputTodo from "./components/InputTodo";
+import ListTodo from "./components/ListTodo";
 
 const App = () => {
   return (
     <div className="p-4">
       <InputTodo />
+      <ListTodo />
     </div>
   );
 };
--- client/src/components/ListTodo.jsx
+++ client/src/components/ListTodo.jsx
@@ -1,11 +1,89 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
 
 const ListTodo = () => {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/todos", {
+          method: "GET",
+        });
+        const jsonData = await response.json();
+        //console.log(jsonData[1]);
+        setTodos(jsonData);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    fetchTodos();
+  }, []);
+  const deleteTodo = async(todo_id) => {
+    try {
+        fetch("")
+    } catch (error) {
+        console.error(error.message);
+    }
+  };
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="mt-10 relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-lg text-left rtl:text-right text-gray-500 ">
+          <thead className="text-sm text-white uppercase bg-green-800">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Todo Description
+              </th>
 
-export default ListTodo
+              <th scope="col" className="px-6 py-3"></th>
+              <th scope="col" className="px-6 py-3">
+                <span className="sr-only"></span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo) => (
+              <tr
+                className="bg-green-300 border-b  hover:bg-green-100 hover:text-black"
+                key={todo.todo_id}
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                >
+                  {todo.description}
+                </th>
+
+                <td className="px-6 py-4 text-center">
+                  <a
+                    href="#"
+                    className="font-medium text-blue-900  hover:underline"
+                  >
+                    Edit
+                  </a>
+                </td>
+                <td className="px-6 py-4 text-center align-middle">
+                  <button onClick={() => deleteTodo(todo.todo_id)}>
+                    <MdDelete className="text-xl text-red-500 mx-auto" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+    // <div className="flex items-center gap-3 py-4">
+    //   <div className="">Description</div>
+    //   <div>
+    //     <FaEdit />
+    //   </div>
+    //   <div>
+    //     <MdDelete />
+    //   </div>
+    // </div>
+  );
+};
+
+export default ListTodo;
